1e00f7a3c36c6294e480c661ae6705881d349f41
TheNick <3235526751@qq.com>
2019-11-30 23:45:07 +0800

    Application update
    1. Add combobox at UI for fundApiType
    2. Move business logic to Models folder
    3. Segregate the input api url from ViewModels
    4. Rename HttpHandler to FundApiHandler
    5. Rename the namespace as FundBasicInfoNavigator

---
+++ src/WpfDataGrid/Models/BasicInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace FundBasicInfoNavigator
+{
+    public class BasicInfo
+    {
+        public string FundCode { get; set; }
+        public string Name { get; set; }
+        public string JZRQ { get; set; }
+        public string DWJZ { get; set; }
+        public string GSZ { get; set; }
+        public string GSZZL { get; set; }
+        public string GzTime { get; set; }
+
+    }
+}
---
+++ src/WpfDataGrid/Models/FundApiHandler.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace FundBasicInfoNavigator
+{
+    public class FundApiHandler
+    {
+        public List<string> LogList { get; set; } = new List<string>();
+
+        public string GetFundApiType(string fundApiType)
+        {
+            switch (fundApiType)
+            {
+                case "EastMoneyFund":
+                    return $"http://fundgz.1234567.com.cn/js/targetFundCode.js?rt=1463558676006";
+                default:
+                    return $"http://fundgz.1234567.com.cn/js/targetFundCode.js?rt=1463558676006";
+            }
+        }
+
+        public void StoreCurrentFundInfo(string fundApiType, string fundCode, ref List<BasicInfo> listObj)
+        {
+            var url = GetFundApiType(fundApiType).Replace("targetFundCode", fundCode);
+            var contents = GetStreamReaderContents(url);
+            var objResult = new BasicInfo();
+            bool searchResult = false;
+
+            if (!string.IsNullOrEmpty(contents))
+            {
+                var convertResult = JsonConvert.DeserializeObject<BasicInfo>(ModifyJsonFormat(contents));
+                if (convertResult != null)
+                {
+                    objResult = convertResult;
+                    searchResult = true;
+                }
+            }
+
+            if (searchResult)
+            {
+                LogList.Add($"Bond Code: {fundCode} success.");
+            }
+            else
+            {
+                objResult.FundCode = fundCode;
+                objResult.Name = "No Found";
+                LogList.Add($"Bond Code: {fundCode} no found.");
+            }
+
+            listObj.Add(objResult);
+        }
+
+        public string GetStreamReaderContents(string url)
+        {
+            var requestObjGet = WebRequest.Create(url);
+
+            try
+            {
+                using (HttpWebResponse webReponseObjGet = (HttpWebResponse)requestObjGet.GetResponse())
+                using (Stream stream = webReponseObjGet.GetResponseStream())
+                using (StreamReader streamReader = new StreamReader(stream))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+            catch (System.Exception)
+            {
+                return string.Empty;
+            }
+        }
+
+        private string ModifyJsonFormat(string strJason)
+        {
+            var startIndex = strJason.IndexOf('(');
+            var endIndex = strJason.IndexOf(')');
+            return strJason.Substring(startIndex + 1, endIndex - startIndex - 1);
+        }
+
+    }
+}
